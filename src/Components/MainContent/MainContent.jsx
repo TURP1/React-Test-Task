8fb526b6e5c10529ffb2826e931de10440fb89ab
TURP1 <turpicrypto@gmail.com>
2023-04-09 13:44:42 +0300

    Revert "Change homePage on NETLIFY"
    This reverts commit faea0e33669cd424c937329f8d315cd310be503f.

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -7,16 +7,10 @@ function MainContent() {
     return (
         <section className={s.main}>
             <div className={s.image_container}>
-                <img className={s.main_img} src={img} srcSet={
-                    `
-                    ${img} 1024w,
-                     ${img} 768w,
-                      ${img} 320w
-                      `
-                } alt="Main bg" />
+                <img className={s.main_img} src={img} alt="Main bg" />
                 <div className={s.image_text}>
-                    <div className={s.image_text_header}>Test assignment for
-                        front-end developer</div>
+                    <div className={s.image_text_header}>Test assignment for 
+                     front-end developer</div>
                     <div className={s.image_text_section}>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</div>
                     <ButtonYellowAnchor buttonName="Sign up" anchor="register"></ButtonYellowAnchor>
                 </div>
